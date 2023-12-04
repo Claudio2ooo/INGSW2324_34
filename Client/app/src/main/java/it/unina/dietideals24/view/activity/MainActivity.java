c7dab5a068cfdc8912049d863ab07775747da084
Salvatore <salvatorefranzese99@gmail.com>
2023-12-04 21:54:50 +0100

    Creazione del progetto Android, configurazione iniziale dei colori del tema e schizzo UI per la Home

---
+++ Client/app/src/main/java/it/unina/dietideals24/view/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package it.unina.dietideals24.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import it.unina.dietideals24.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
