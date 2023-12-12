e20ae6d69f4de2ea0fcf190b42c61e37b02f4fcf
Salvatore <salvatorefranzese99@gmail.com>
2023-12-12 11:26:44 +0100

    Aggiunta logica per mostrare le icone delle categorie sull'interfaccia utente

--- Client/app/src/main/java/it/unina/dietideals24/view/fragment/HomeFragment.java
+++ Client/app/src/main/java/it/unina/dietideals24/view/fragment/HomeFragment.java
@@ -41,9 +41,7 @@ public class HomeFragment extends Fragment {
     private void initCategory(View view) {
         ArrayList<CategoryItem> categories = new ArrayList<>();
 
-        for (CategoryEnum category : CategoryEnum.values()) {
-            categories.add(new CategoryItem(capitalize(category.toString()), R.drawable.round_arrow_forward_24));
-        }
+        getCategoryEnum(categories);
 
         RecyclerView recyclerViewCategories = view.findViewById(R.id.categoryList);
         recyclerViewCategories.setLayoutManager(new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false));
@@ -52,7 +50,32 @@ public class HomeFragment extends Fragment {
         recyclerViewCategories.setAdapter(categoryAdapter);
     }
 
+    /**
+     * This method takes all the constants in the CategoryEnum and initializes the list categories array
+     * @param categories containing the categories to be shown in the user interface
+     * */
+    private void getCategoryEnum(ArrayList<CategoryItem> categories) {
+        CategoryEnum[] values = CategoryEnum.values();
+        for (int i = 0; i < values.length && i < 6; i++) {
+            CategoryEnum category = values[i];
+            categories.add(new CategoryItem(capitalize(category.toString()), iconCategory(category.toString())));
+        }
+    }
+
+    /**
+     * This method converts the first character of a given string to uppercase; while the remainder is lowercase
+     * @param str string to capitalise
+     * */
     private String capitalize(String str) {
         return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
+    }
+
+    /**
+     * this method automatically sets the icon string of a category, based on the input parameter, and returns it
+     * @param category category name
+     * */
+    private int iconCategory(String category) {
+        String iconName = "round_" + category.toLowerCase() + "_24";
+        return getResources().getIdentifier(iconName, "drawable", getActivity().getPackageName());
     }
 }
