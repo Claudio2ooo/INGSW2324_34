c792374df5badc7c7f86028caa83a8f671e02764
Salvatore <salvatorefranzese99@gmail.com>
2023-12-18 20:25:09 +0100

    Aggiunta UI per la registrazione e apportato modifiche alla UI "AuctionDeatails"

--- Client/app/src/main/java/it/unina/dietideals24/view/activity/SignInActivity.java
+++ Client/app/src/main/java/it/unina/dietideals24/view/activity/SignInActivity.java
@@ -2,31 +2,30 @@ package it.unina.dietideals24.view.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import it.unina.dietideals24.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_sign_in);
 
         InitView();
     }
 
     private void InitView() {
-        TextView signInBtn = findViewById(R.id.signInBtn);
+        ImageView backBtn = findViewById(R.id.backBtn);
 
-        signInBtn.setOnClickListener(new View.OnClickListener() {
+        backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent signInActivity = new Intent(getApplicationContext(), SignInActivity.class);
-                startActivity(signInActivity);
+                finish();
             }
         });
     }
