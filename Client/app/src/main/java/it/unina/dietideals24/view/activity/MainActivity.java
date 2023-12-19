672f4d6c34cf476ca646c2de0905ec1355929dc7
Salvatore <salvatorefranzese99@gmail.com>
2023-12-19 17:20:29 +0100

    Aggiunto pop-up per la creazione di un asta (all'inglese o al ribasso), iniziato a creare UI del profilo e funzione back tra fragment

--- Client/app/src/main/java/it/unina/dietideals24/view/activity/MainActivity.java
+++ Client/app/src/main/java/it/unina/dietideals24/view/activity/MainActivity.java
@@ -1,9 +1,16 @@
 package it.unina.dietideals24.view.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.fragment.app.Fragment;
 
 import it.unina.dietideals24.R;
 import it.unina.dietideals24.databinding.ActivityMainBinding;
@@ -21,28 +28,59 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        replaceFragment(new HomeFragment());
+        replaceFragment(new HomeFragment(), "HOME");
 
         binding.bottomNavigation.setOnItemSelectedListener(item -> {
 
             int itemId = item.getItemId();
             if (itemId == R.id.nav_home) {
-                replaceFragment(new HomeFragment());
+                replaceFragment(new HomeFragment(), "HOME");
             } else if (itemId == R.id.nav_auction) {
-                replaceFragment(new AuctionFragment());
+                replaceFragment(new AuctionFragment(), "AUCTION");
             } else if (itemId == R.id.nav_add) {
-                // TODO StartActivity AddAuction
+                showAddDialog();
             } else if (itemId == R.id.nav_notify) {
-                replaceFragment(new NotifyFragment());
+                replaceFragment(new NotifyFragment(), "NOTIFY");
             } else if (itemId == R.id.nav_profile) {
-                replaceFragment(new ProfileFragment());
+                replaceFragment(new ProfileFragment(), "PROFILE");
             }
 
             return true;
         });
     }
 
-    private void replaceFragment(Fragment fragment) {
-        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
+    private void replaceFragment(Fragment fragment, String tag) {
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment, tag).addToBackStack(tag).commit();
+    }
+
+    private void showAddDialog() {
+        ConstraintLayout addAuctionConstraintLayout = findViewById(R.id.addAuctionConstraintLayout);
+        View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.create_dialog, addAuctionConstraintLayout);
+
+        Button englishAuctionBtn = view.findViewById(R.id.englishAuctionBtn);
+        Button reverseAuctionBtn = view.findViewById(R.id.downwardAuctionBtn);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        englishAuctionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        reverseAuctionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        alertDialog.show();
     }
 }
