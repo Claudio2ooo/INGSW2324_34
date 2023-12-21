b412cc8e12406800b221bc847fcf628f075b5c8a
Salvatore <salvatorefranzese99@gmail.com>
2023-12-21 12:53:00 +0100

    Conclusa UI profilo
    Aggiunto il pop-up per la modifica dei dati del profilo utente con incluso il photo picker

--- Client/app/build.gradle.kts
+++ Client/app/build.gradle.kts
@@ -38,11 +38,15 @@ android {
 dependencies {
 
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Image Picker
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+    implementation("androidx.activity:activity:1.8.2")
 
     // Retrofit Dependencies
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
--- Client/app/src/main/java/it/unina/dietideals24/view/fragment/ProfileFragment.java
+++ Client/app/src/main/java/it/unina/dietideals24/view/fragment/ProfileFragment.java
@@ -3,18 +3,30 @@ package it.unina.dietideals24.view.fragment;
 import android.app.AlertDialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.PickVisualMediaRequest;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
 
 import it.unina.dietideals24.R;
 
 public class ProfileFragment extends Fragment {
+
+    ImageView imageProfile;
+    ActivityResultLauncher<PickVisualMediaRequest> singlePhotoPickerLauncher;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -32,6 +44,10 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         Button changePasswordBtn = view.findViewById(R.id.changePasswordBtn);
+        Button editProfileBtn = view.findViewById(R.id.editProfileBtn);
+
+        initializeSinglePhotoPickerLauncher();
+
         changePasswordBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -39,7 +55,27 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+        editProfileBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editProfileDialog(view);
+            }
+        });
+
         return view;
+    }
+
+    private void initializeSinglePhotoPickerLauncher() {
+        singlePhotoPickerLauncher = registerForActivityResult(new ActivityResultContracts.PickVisualMedia(), new ActivityResultCallback<Uri>() {
+            @Override
+            public void onActivityResult(Uri uri) {
+                if (uri == null) {
+                    Toast.makeText(getContext(), "Seleziona immagine!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Glide.with(getContext()).load(uri).into(imageProfile);
+                }
+            }
+        });
     }
 
     private void changePasswordDialog(View view) {
@@ -55,6 +91,33 @@ public class ProfileFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 alertDialog.dismiss();
+            }
+        });
+
+        if (alertDialog.getWindow() != null) {
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        alertDialog.show();
+    }
+
+    private void editProfileDialog(View view) {
+        ConstraintLayout editProfileConstraintLayout = view.findViewById(R.id.editProfileConstraintLayout);
+        View viewEditProfileDialog = LayoutInflater.from(getContext()).inflate(R.layout.edit_profile_dialog, editProfileConstraintLayout);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setView(viewEditProfileDialog);
+        final AlertDialog alertDialog = builder.create();
+
+        Button changeImgBtn = viewEditProfileDialog.findViewById(R.id.changeImgBtn);
+        imageProfile = viewEditProfileDialog.findViewById(R.id.imageProfile);
+
+        changeImgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Launch the photo picker and let the user choose only images
+                singlePhotoPickerLauncher.launch(new PickVisualMediaRequest.Builder()
+                        .setMediaType(ActivityResultContracts.PickVisualMedia.ImageOnly.INSTANCE)
+                        .build());
             }
         });
 
