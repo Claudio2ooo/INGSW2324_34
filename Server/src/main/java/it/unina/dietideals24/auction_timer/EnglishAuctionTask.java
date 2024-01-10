d091ef18183785408d09c030a11b42a0aa215df3
Claudio2ooo <riccioclaudio2000@gmail.com>
2024-01-10 12:37:56 +0100

    fix interface

--- Server/src/main/java/it/unina/dietideals24/auction_timer/EnglishAuctionTask.java
+++ Server/src/main/java/it/unina/dietideals24/auction_timer/EnglishAuctionTask.java
@@ -2,6 +2,7 @@ package it.unina.dietideals24.auction_timer;
 
 import it.unina.dietideals24.model.Auction;
 import it.unina.dietideals24.service.implementation.EnglishAuctionService;
+import it.unina.dietideals24.service.interfaces.IEnglishAuctionService;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,10 +12,10 @@ import java.util.TimerTask;
 @Setter
 public class EnglishAuctionTask extends TimerTask {
 
-    private EnglishAuctionService englishAuctionService;
+    private IEnglishAuctionService englishAuctionService;
     private Auction auction;
 
-    public EnglishAuctionTask(EnglishAuctionService englishAuctionService, Auction auction) {
+    public EnglishAuctionTask(IEnglishAuctionService englishAuctionService, Auction auction) {
         this.englishAuctionService = englishAuctionService;
         this.auction = auction;
     }
