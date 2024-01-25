eb8ca7f8c6438d44fcd4d6a873d2b2b2a9b50d8d
Claudio Riccio <riccioclaudio2000@gmail.com>
2024-01-25 17:33:52 +0100

    Fix typo in properties e creazione cartelle all'avvio del server

--- Server/src/main/java/it/unina/dietideals24/ServerApplication.java
+++ Server/src/main/java/it/unina/dietideals24/ServerApplication.java
@@ -3,9 +3,19 @@ package it.unina.dietideals24;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.File;
+
 @SpringBootApplication
 public class ServerApplication {
     public static void main(String[] args) {
+        createNecessaryDirectories();
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    private static void createNecessaryDirectories() {
+        new File("images").mkdir();
+        new File("images/user").mkdir();
+        new File("images/english_auction").mkdir();
+        new File("images/downward_auction").mkdir();
     }
 }
